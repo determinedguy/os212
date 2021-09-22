79a5312b2b5c127c928aad2568aad34948c92644
Muhammad Athallah <28977286+determinedguy@users.noreply.github.com>
2021-09-22 17:35:48 +0700

    Update myscript.sh

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
+# REV01 Wed 22 Sep 2021 17:34:10 WIB
 # START Thu 16 Sep 2021 13:13:20 WIB
 
+REC2="mhd.athallah@gmail.com"
+REC1="operatingsystems@vlsm.org"
 FILES="my*.txt my*.sh"
 SHA="SHA256SUM"
+
+[ -d $HOME/RESULT/ ] || mkdir -p $HOME/RESULT/
+pushd $HOME/RESULT/
+for II in W?? ; do
+    [ -d $II ] || continue
+    TARFILE=my$II.tar.bz2
+    TARFASC=$TARFILE.asc
+    rm -f $TARFILE $TARFASC
+    echo "tar cfj $TARFILE $II/"
+    tar cfj $TARFILE $II/
+    echo "gpg --armor --output $TARFASC --encrypt --recipient $REC1 --recipient $REC2 $TARFILE"
+    gpg --armor --output $TARFASC --encrypt --recipient $REC1 --recipient $REC2 $TARFILE
+done
+popd
+
+for II in $HOME/RESULT/myW*.tar.bz2.asc ; do
+   echo "Check and move $II..."
+   [ -f $II ] && mv -f $II .
+done
 
 echo "rm -f $SHA $SHA.asc"
 rm -f $SHA $SHA.asc
